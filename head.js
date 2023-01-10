380601d2914e83d75eee06e3ad606a718ab229b2
Maryan <maryanay2000@gmail.com>
2023-01-09 20:15:37 -0500

    eslinted files

--- head.js
+++ head.js
@@ -5,11 +5,10 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
   }
 };
-function head(array) {
+
+const head = function(array) {
   return array[0];
-}
-
-
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- tail.js
+++ tail.js
@@ -5,29 +5,29 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
   }
 };
-function tail(array) {
-  let newArray = []
+const tail = function(array) {
+  let newArray = [];
 
-  for (i = 1; i < array.length; i++){
+  for (let i = 1; i < array.length; i++) {
     newArray.push[i];
   }
   return newArray;
-}
+};
 
-const test1 = tail([1, 2, 3]); 
+const test1 = tail([1, 2, 3]);
 assertEqual(test1, [1, 2, 3]);
-console.log(test1)
+console.log(test1);
 
-const test2 = tail(["Hello", "Lighthouse", "Labs"]); 
+const test2 = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(test2, ["Lighthouse", "Labs"]);
 console.log(test2);
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs")
+assertEqual(result[1], "Labs");
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
